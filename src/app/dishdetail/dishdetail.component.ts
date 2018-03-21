8e362ef990dbc8eee13f299ab614ce0c0bd81482
Charlton Smith <charltonsmith@hotmail.com>
2018-03-21 14:34:54 -0700

    first commit

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Dish } from '../shared/dish';
 
 
 const DISH = {
@@ -48,8 +49,8 @@ const DISH = {
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-
-  dish = DISH;
+  @Input()
+  dish: Dish;
   
   constructor() { }
 
