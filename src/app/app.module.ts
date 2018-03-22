cf9f41e2217dabfc676e7b4cca7f581636c65d6b
Charlton Smith <charltonsmith@hotmail.com>
2018-03-22 15:27:37 -0700

    basic service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,10 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
 
 @NgModule({
+  
   declarations: [
     AppComponent,
     MenuComponent,
@@ -23,7 +25,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MaterialModule,
     FlexLayoutModule   
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
